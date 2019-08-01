0ae301d4b7fa7262a9191a4a5f52737f84201379
cc <1060938177qq.com>
2019-08-01 12:03:46 +0800

    添加home

--- src/main.js
+++ src/main.js
@@ -2,8 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import iView from 'iview'
+import config from '@/config'
 
+import './index.less'
+import '@/assets/icons/iconfont.css'
+
+if (process.env.NODE_ENV !== 'production') require('@/mock')
+
+Vue.use(iView)
+
+/**
+ * @description 生产环境关掉提示
+ */
 Vue.config.productionTip = false
+
+/**
+ * @description 全局注册应用配置
+ */
+Vue.prototype.$config = config
 
 new Vue({
   router,
--- src/router/routes.js
+++ src/router/routes.js
@@ -16,8 +16,30 @@
  * }
  */
 
+import Main from '@/components/main'
+
 export default [
   {
-
+    path: '/',
+    name: '_home',
+    redirect: '/home',
+    component: Main,
+    meta: {
+      hideInMenu: true,
+      notCache: true
+    },
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        meta: {
+          hideInMenu: true,
+          title: '首页',
+          notCache: true,
+          icon: 'md-home'
+        },
+        component: () => import('@/view/single-page/home')
+      }
+    ]
   }
 ]
